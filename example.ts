9a5e3c2092eff28791854f9a3b3d33e6b474900c
Meirion Hughes <mhughes@geologging.com>
2017-09-12 11:45:00 +0100

    use updated levelup, abstract typings

--- example.ts
+++ example.ts
@@ -2,15 +2,22 @@ import './src';
 import * as levelup from 'levelup';
 import * as leveldown from 'leveldown';
 import * as encode from 'encoding-down';
+import * as Abstract from 'abstract-leveldown';
 
-let db = levelup(encode(leveldown('./db')));
+type CustomDown = Abstract.LevelDOWN<string, any, any, any, any, any, { lte: string },any >
+
+let store = encode(leveldown('./db')) as CustomDown;
+
+let db = levelup(store);
 
 async function main() {
-  await db.put("hello", "world");
+  await db.put("hello", "world", );
 
-  let iter = db.iterator();
+  let iter = db.iterator({}); // lte required in typing^
 
   for await (let [key, value] of iter) {
+
+
     console.log(key, value);
   }
 }
